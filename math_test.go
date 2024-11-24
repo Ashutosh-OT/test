9fb157e092b26acd9e3ffc1272bf558569ccae93
Ashutosh-OT <ashutosh.tripathi@opstree.com>
2024-11-25 04:35:23 +0530

    Update math_test.go

--- math_test.go
+++ math_test.go
@@ -2,24 +2,12 @@ package main
 
 import "testing"
 
-// addTest represents a test case for the Add function
-type addTest struct {
-	arg1, arg2, expected int
-}
+func TestAdd(t *testing.T){
 
-// Define test cases for the Add function
-var addTests = []addTest{
-	{2, 3, 5},
-	{4, 8, 12},
-	{6, 9, 15},
-	{3, 10, 13},
-}
+    got := Add(4, 6)
+    want := 10
 
-func TestAdd(t *testing.T) {
-	for _, test := range addTests {
-		output := Add(test.arg1, test.arg2)
-		if output != test.expected {
-			t.Errorf("Add(%d, %d) = %d; want %d", test.arg1, test.arg2, output, test.expected)
-		}
-	}
+    if got != want {
+        t.Errorf("got %q, wanted %q", got, want)
+    }
 }
