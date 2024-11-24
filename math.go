702eeb119efe38be28f16be57224f1fdcd4d8b71
Ashutosh-OT <ashutosh.tripathi@opstree.com>
2024-11-25 04:36:48 +0530

    Update math.go

--- math.go
+++ math.go
@@ -1,11 +1,11 @@
-package main
+package math
 
 // Add is our function that sums two integers
-func Add(x, y int) int {
-    return x + y
+func Add(x, y int) (res int) {
+	return x + y
 }
 
 // Subtract subtracts two integers
-func Subtract(x, y int) int {
-    return x - y
+func Subtract(x, y int) (res int) {
+	return x - y
 }
